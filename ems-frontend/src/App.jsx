61900a4742003860d6077076b3f9e8eec57a5086
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-05 00:35:53 +0530

    fixed routes

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,14 +2,18 @@ import "./App.css";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
-import HelloWorld from "./HelloWorld";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
