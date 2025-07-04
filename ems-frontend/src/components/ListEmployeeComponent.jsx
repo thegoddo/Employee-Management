c8895bd7454aac254ab19ba43ac79f8e934e2585
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-05 00:28:14 +0530

    added list employee component

---
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { listEmployees } from "../services/EmployeeService";
+
+const ListEmployeeComponent = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="text-center">List Of Employees</h2>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Employee First Name</th>
+            <th>Employee Last Name</th>
+            <th>Employee Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {" "}
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListEmployeeComponent;
